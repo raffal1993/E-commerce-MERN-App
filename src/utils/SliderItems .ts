7f89153462d5fe08220e315f3800901067b2c87e
raffal1993 <raffal1993@gmail.com>
2022-09-29 19:45:06 +0200

    add: static data files

---
+++ src/utils/SliderItems .ts
@@ -0,0 +1,23 @@
+export const sliderItems = [
+  {
+    id: 1,
+    img: 'https://images.unsplash.com/photo-1555529771-122e5d9f2341?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80',
+    title: 'SUMMER SALE',
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: '7CC2B9',
+  },
+  {
+    id: 2,
+    img: 'https://i.picsum.photos/id/835/600/900.jpg?hmac=QrR_emFgEg6gpoCKLYixJIqYbm3SOw8FvGbCyHOKqQA',
+    title: 'AUTUMN COLLECTION',
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: 'A5C27C',
+  },
+  {
+    id: 3,
+    img: 'https://images.unsplash.com/photo-1441984904996-e0b6ba687e04?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+    title: 'LOUNGEWEAR LOVE',
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: '937CC2',
+  },
+];
